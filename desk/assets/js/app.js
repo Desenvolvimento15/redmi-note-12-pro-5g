db2285b7b399ed7af87590023e005d95698b7b68
desenvolvimento15 <desenvolvimento15@dl.com.br>
2023-04-10 16:23:07 -0300

    vs01

--- desk/assets/js/app.js
+++ desk/assets/js/app.js
@@ -1,12 +1,20 @@
 gsap.registerPlugin(ScrollTrigger);
-const tl = gsap.timeline();
-tl.to(".pin-spacer",5,{x:-window.innerWith})
 
-ScrollTrigger.create({
-    Animation: tl,
-    trigger:".pin-spacer",
-    start:"center center",
-    end:"+=4000",
-    scrub:true,
-    pin:true
-})
+gsap.utils.toArray(".img-box").forEach(section => {
+	let tl = gsap.timeline({
+			scrollTrigger: {
+				trigger: section,
+				start: "center center",
+   // faz com que a altura da rolagem (durante a fixação) coincida com a largura, assim a velocidade permanece constante (vertical/horizontal)
+				end: () => "+=" + section.offsetWidth, 
+				scrub: true,
+				pin: true,
+                anticipatePin: 3,
+			},
+			defaults: {ease: "none"}
+		});
+	// animate the container one way...
+	tl.fromTo(section.querySelector(".image90"), { xPercent: 100, x: 0}, {xPercent: 0})
+	  // ...and the image the opposite way (at the same time)
+	  .fromTo(section.querySelector(".image90 img"), {xPercent: 0, x: 0}, {xPercent: 0}, 0);
+});
