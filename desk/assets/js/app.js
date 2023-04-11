a2e283fbbea6979bac660eb555e7da8dbca0619c
desenvolvimento15 <desenvolvimento15@dl.com.br>
2023-04-11 13:48:01 -0300

    vs01

--- desk/assets/js/app.js
+++ desk/assets/js/app.js
@@ -1,5 +1,28 @@
 gsap.registerPlugin(ScrollTrigger);
 
+gsap.registerPlugin(ScrollTrigger);
+gsap.utils.toArray(".section02").forEach(div => {
+
+	let tl = gsap.timeline({
+		    duration: 1,
+			scrollTrigger: {
+			trigger: div,
+			start: "top top",
+			end: "100% top",
+			pin: !0,
+            scrub: !0,
+			anticipatePin: 3,
+			},
+		
+			
+		});
+	
+		tl.to(".section02 .div43", { opacity: 1 })
+		.to(".section02 .div50", { opacity: 1 })
+		.to(".section02 .div53", { opacity: 1 })
+
+});
+
 gsap.utils.toArray(".img-box").forEach(section => {
 
 	let tl = gsap.timeline({
@@ -19,7 +42,7 @@ gsap.utils.toArray(".img-box").forEach(section => {
 	      .from(section.querySelector(".image91"), {xPercent: -105, x:0}, {xPercent: 0}, 0);
 });
 
-
+gsap.registerPlugin(ScrollTrigger);
 gsap.utils.toArray(".div116").forEach(section => {
 
 	let tl = gsap.timeline({
@@ -32,10 +55,11 @@ gsap.utils.toArray(".div116").forEach(section => {
             scrub: !0,
 			anticipatePin: 3,
 			},
-		
+			defaults: {ease: "none"}
 			
 		});
 	
 	    tl.from(".image123", { x: "0", y: "0" });
 
 });
+
